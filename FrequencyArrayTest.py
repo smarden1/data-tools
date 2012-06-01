6209c4b8ff2064da1955a5712bb7373c48b85d8d
Steve Mardenfeld <smardenfeld@etsy.com>
2012-05-31 21:45:22 -0400

    initial tests for frequency array

---
+++ FrequencyArrayTest.py
@@ -0,0 +1,38 @@
+import unittest
+from FrequencyArray import FrequencyArray
+
+class TestEmptyFrequencyArray(unittest.TestCase):
+
+    def setUp(self):
+        self.empty_fa = FrequencyArray()
+
+        self.ranged_fa = FrequencyArray()
+        for i in range(10, 0, -1):
+            self.ranged_fa.add(i)
+
+    def test_no_data_to_start(self):
+        self.assertEqual(self.empty_fa.data, [])
+
+    def test_add_data(self):
+        self.empty_fa.add(1)
+        self.empty_fa.add(3)
+
+        self.assertEqual(self.empty_fa.data, [1, 3])
+
+    def test_add_data_with_counts(self):
+        self.empty_fa.add(1, 2)
+        self.empty_fa.add(3)
+
+        self.assertEqual(self.empty_fa.data, [1, 1, 3])
+
+    def test_ordered_data(self):
+        self.assertEqual(self.ranged_fa.ordered_data(), range(1, 11))
+
+    def test_max(self):
+        self.assertEqual(self.ranged_fa.max, 10)
+
+    def test_min(self):
+        self.assertEqual(self.ranged_fa.max, 1)
+
+if __name__ == "__main__":
+    unittest.main()
