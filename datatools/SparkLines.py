8a5677bf68038273b20cf9d22ff55aa22fc055d5
Steve Mardenfeld <smardenfeld@etsy.com>
2012-06-05 23:19:56 -0400

    redid sparkline to work with frequencies

--- datatools/SparkLines.py
+++ datatools/SparkLines.py
@@ -21,25 +21,11 @@ class SparkLines(object):
 
     def __init__(self, data, zero_start = False):
         self.data = data
-
-        try:
-            self.max = data.max
-            self.min = data.min
-            self.n = data.n
-        except AttributeError:
-            self.max = max(self.data)
-            self.min = min(self.data)
-            self.n = len(self.data)
-        finally:
-            self.min = 0 if zero_start else self.min
-            self.spread = self.max - self.min
-            self.bin_size = math.ceil(self.spread / self.range) + 1
+        self.bin_size = math.ceil(self.data.range(zero_start) / self.range) + 1
 
     def result(self):
-        if hasattr(self.data, "explode"):
-            return (self.getBar(i) for i in self.data.explode())
-        return (self.getBar(i) for i in self.data)
+        return (self.getBar(i) for i in self.data.explode())
 
     def getBar(self, value):
-        i = int(math.floor((value - self.min)/self.bin_size))
+        i = int(math.floor((value - self.data.min) / self.bin_size))
         return self.bars[i]
