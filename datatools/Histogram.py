e0a8efb90e52ca7f134922085fd517b61c3c73a9
Steve Mardenfeld <smardenfeld@etsy.com>
2012-06-07 07:15:24 -0400

    fixed histograms

--- datatools/Histogram.py
+++ datatools/Histogram.py
@@ -6,18 +6,26 @@ class Histogram(FrequencyTable):
     def __init__(self, bin_width = None):
         super(Histogram, self).__init__()
         self.bin_width = float(bin_width)
+        self.max_bin_key = 0
 
     def add(self, key, count = 1):
-        bin_key = int(math.floor(key / self.bin_width))
+        bin_key = self.bin_key(key)
         self.data[bin_key] += count
         self.total += key * count
         self.n += count
+        self.max_bin_key = max(self.max_bin_key, bin_key)
         self.max = max(self.max, key)
         self.min = min(self.min, key)
         self.is_sorted = False
 
+    def bin_key(self, key):
+        return int(math.floor(key / self.bin_width))
+
+    def bin_key_to_value(self, key):
+        return int(key * self.bin_width)
+
     def ordered_data(self):
         if not self.is_sorted:
-            self.ordered_data_list = [(i, self.data[i]) for i in xrange(0, self.max, self.bin_width)]
+            self.ordered_data_list = [(self.bin_key_to_value(i), self.data[i]) for i in xrange(self.max_bin_key + 1)]
             self.is_sorted = True
         return self.ordered_data_list
