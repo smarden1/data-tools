32fa21ccde0b0db86d052b04a977827eb45ac6cf
steve <smarden1@gmail.com>
2012-06-07 22:49:52 -0400

    fixes to moments

--- datatools/Moments.py
+++ datatools/Moments.py
@@ -7,23 +7,23 @@ class Moments(object):
     __metaclass__ = ABCMeta
 
     def mean(self):
-        return self.moments(1) / self.n
+        return self.moment(1) / self.n
 
     def average(self):
         return self.mean()
 
     def variance(self):
-        return (self.moments(2)) - (self.mean() ** 2)
+        return (self.moment(2)) - (self.mean() ** 2)
 
     def std(self):
         return math.sqrt(self.variance)
 
     def skew(self):
-        return ((self.moments(3) / self.n) - (3 * self.mean() * self.variance) - self.mean ** 3) / (self.std **3)
+        return ((self.moment(3) / self.n) - (3 * self.mean() * self.variance) - self.mean ** 3) / (self.std **3)
 
     # http://www.ats.ucla.edu/stat/mult_pkg/faq/general/kurtosis.htm    
     def kurtosis(self):
-        return ((self.moments(4) - (self.mean ** 4)) / self.n) / ((self.variance / n) ** 2) - 3
+        return ((self.moment(4) - (self.mean ** 4)) / self.n) / ((self.variance / n) ** 2) - 3
 
     @abstractmethod
     def moment(self, m):
--- datatools/StreamingMoments.py
+++ datatools/StreamingMoments.py
@@ -1,12 +1,18 @@
-import datatools.Moments
+from datatools.Moments import Moments
 
-class StreamingMoments(Moments.Moments):
+class StreamingMoments(Moments):
 
     def __init__(self):
         self.n = 0
-        self.moments = [0] * 4
+        self.total = 0
+        self.moments_data = [0] * 6
+        self.__moment_count = range(len(self.moments_data))
 
     def add(self, data_point):
         self.n += 1
-        for i in self.moments:
-            self.moments[i] += data_point ** i
+        self.total += data_point
+        for i in self.__moment_count:
+            self.moments_data[i] += data_point ** i
+
+    def moment(self, n):
+        return self.moments_data[n]
