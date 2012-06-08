2eb29f70987dbc10b5c34128b49b2c2ae01ce886
steve <smarden1@gmail.com>
2012-06-07 23:15:20 -0400

    bootstrap

--- datatools/Bootstrap.py
+++ datatools/Bootstrap.py
@@ -1,5 +1,7 @@
 import random
 from datatools.FrequencyBase import FrequencyBase
+from datatools.StreamingMoments import StreamingMoments
+from datatools.FrequencyArray import FrequencyArray
 
 class BootStrap(object):
 
@@ -9,10 +11,33 @@ class BootStrap(object):
 
         self.sample_size = int(kwargs.get("sample_size", frequency.n))
         self.resample_count = int(kwargs.get("resample_count", frequency.n))
-        self.functions = args or ["mean"]
         self.ci = float(kwargs.get("ci", .025))
 
+        self.functions = args or ["mean"]
+        self.function_results = [FrequencyArray()) for i in self.functions]
+
+        if kwargs.has_key("seed"):
+            random.seed(kwargs.get("seed"))
 
     def run(self):
-        #first make the streaming percentile
-        pass
+        for n in self.resample_count():
+            for stats in self.run_sample_statistics():
+                for i, s in enumerate(stats):
+                    self.functions[i].add(s)
+
+        # maybe print out graph here too
+        return [self.confidence_interval(i) for i in self.functions]
+
+    def confidence_interval(self, freq_array):
+        return (freq_array.percentile(.5 - self.ci), freq_array.percentile(.5 + self.ci))
+
+    def run_sample_statistics(self):
+        moments = StreamingMoments() # see if i can attach streamingMoment to same object
+
+        for i in self.sample_size:
+            moments.add(self.random_with_replacement())
+
+        return map(lambda a: getattr(moments, a)(), self.functions)
+
+    def random_with_replacement(self):
+        yield random.choice(self.frequency.explode())
